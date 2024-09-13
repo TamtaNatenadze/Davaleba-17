0411e3ba9aab9259c5f72e0875dc618879937e60
TamtaNatenadze <tamta.natenadze.1@iliauni.edu.ge>
2024-09-13 05:57:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function compareWords(word1, word2) {
+    let length1 = word1.length;
+    let length2 = word2.length;
+    if (length1 === length2) {
+        alert(true);
+    } else {
+        alert(false);
+    }
+}
+compareWords("Javascript", "Python");
+
